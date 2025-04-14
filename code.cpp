10e77df43dc84f89671ccfb9911a13a22df53a3b
dwaara2rawwartaw <kaasp45@gmail.com>
2025-04-14 10:47:32 +0000

    убрал код

---
+++ code.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int first, second;
+    string x;
+    if (x=="+")
+    {
+        cout << first + second << endl;
+    }
+    if (x=="-")
+    {
+        cout << first - second << endl;
+    }
+    if (x=="*")
+    {
+        cout << first * second << endl;
+    }
+    if (x=="/")
+    {
+        cout << first / second << endl;
+    }
+}
